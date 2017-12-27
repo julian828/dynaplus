4221cb42f2f8f52b656b7542a3cac46a3c9f6e3c
julian828 <leonzhu828@hotmail.com>
2017-12-27 14:06:43 -0500

    add login#

--- dynaplus/settings.py
+++ dynaplus/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     
     '192.168.56.101',
+    'localhost',
     
     ]
 
--- dynaplus/urls.py
+++ dynaplus/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dynatag/', include('dynatag.urls')),
+    path('account/', include('dynatag.urls')),
 ]
